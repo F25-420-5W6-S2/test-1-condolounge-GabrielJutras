f7a68db005507ba9ebdd96d629d6620fef867d52
Jutras, Gabriel <gabrieljutras01@gmail.com>
2025-11-18 13:06:53 -0500

    Both repo methods complete

--- CondoLounge/Data/Repositories/CondoLoungeApplicationUserRepository.cs
+++ CondoLounge/Data/Repositories/CondoLoungeApplicationUserRepository.cs
@@ -1,12 +1,29 @@
 ﻿using CondoLounge.Data.Entities;
 using CondoLounge.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace CondoLounge.Data.Repositories
 {
+    
+
     public class CondoLoungeApplicationUserRepository : CondoLoungeGenericGenericRepository<ApplicationUser>, ICondoLoungeApplicationUserRepository
     {
+        protected readonly ApplicationDbContext _db;
+        protected readonly ILogger<CondoLoungeGenericGenericRepository<ApplicationUser>> _logger;
+
         public CondoLoungeApplicationUserRepository(ApplicationDbContext db, ILogger<CondoLoungeGenericGenericRepository<ApplicationUser>> logger) : base(db, logger)
         {
+            _db = db;
+            _logger = logger;
+        }
+
+        public IEnumerable<ApplicationUser> GetUsersForBuilding(int buildingId)
+        {
+            return _db.Condos
+                .Where(c => c.BuildingId == buildingId)
+                .SelectMany(c => c.Users)
+                .Distinct()
+                .ToList();
         }
     }
 }
--- CondoLounge/Data/Repositories/CondoLoungeCondoRepository.cs
+++ CondoLounge/Data/Repositories/CondoLoungeCondoRepository.cs
@@ -1,12 +1,26 @@
 ﻿using CondoLounge.Data.Entities;
 using CondoLounge.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace CondoLounge.Data.Repositories
 {
     public class CondoLoungeCondoRepository : CondoLoungeGenericGenericRepository<Condo>, ICondoLoungeCondoRepository
     {
+        protected readonly ApplicationDbContext _db;
+        protected readonly ILogger<CondoLoungeGenericGenericRepository<Condo>> _logger;
+
         public CondoLoungeCondoRepository(ApplicationDbContext db, ILogger<CondoLoungeGenericGenericRepository<Condo>> logger) : base(db, logger)
         {
+            _db = db;
+            _logger = logger;
+        }
+
+        public IEnumerable<Condo> GetCondosForBuilding(int buildingId)
+        {
+            return _db.Condos
+                .Include(c => c.Building)
+                .Where(c => c.BuildingId == buildingId)
+                .ToList();
         }
     }
 }
